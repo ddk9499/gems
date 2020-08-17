e1edea99eba064b6b68416bdb0198939c1b225c5
Dostonbek Kamalov <ddk9499@gmail.com>
2020-08-17 18:18:25 +0500

    Add AGP into Deps

--- includedBuild/dependencies/src/main/kotlin/uz/dkamaloff/gems/dependencies/Deps.kt
+++ includedBuild/dependencies/src/main/kotlin/uz/dkamaloff/gems/dependencies/Deps.kt
@@ -4,9 +4,14 @@ object Deps {
     private const val kotlinVersion = "1.3.72"
 
     val kotlin = Kotlin("org.jetbrains.kotlin")
+    val android = Android
 
     class Kotlin(groupName: String) {
         val stdlib = "$groupName:kotlin-stdlib:$kotlinVersion"
         val plugin = "$groupName:kotlin-gradle-plugin:$kotlinVersion"
+    }
+
+    object Android {
+        const val plugin = "com.android.tools.build:gradle:3.6.0"
     }
 }
